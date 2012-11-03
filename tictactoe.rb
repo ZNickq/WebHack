aceefd41ac9ec7fc0399095531d0f0bfd09527a4
ZNickq <teapa011@gmail.com>
2012-11-03 12:12:03 +0200

    RED

--- tictactoe.rb
+++ tictactoe.rb
@@ -3,7 +3,9 @@ require 'test/unit'
 class TTTTest < Test::Unit::TestCase
 
 	def setup
-		@player_test = TicTacToePlayer.new "X"
+		@player_test = TicTacToePlayer.new "Testy"
+		@player_x = TicTacToePlayer.new "X"
+		@player_0 = TicTacToePlayer.new "0"
 	end
 
 	def test_one_argument
@@ -19,7 +21,13 @@ class TTTTest < Test::Unit::TestCase
 	end
 
 	def test_new_player
-		assert_equal @player_test.name, "X"
+		assert_equal @player_test.name, "Testy"
+	end
+
+	def test_board_init
+		game_state = @player_x.turn(Hash.new)
+		@player_0.turn(game_state)
+		assert game_state["owned_by_"+@player_x.name] != nil && game_state["owned_by_"+@player_0.name] != nil
 	end
 
 
