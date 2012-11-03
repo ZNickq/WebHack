893018a2a793880bf9de736b3cc1d09ce8f97f2d
ZNickq <teapa011@gmail.com>
2012-11-03 11:45:21 +0200

    RED

--- tictactoe.rb
+++ tictactoe.rb
@@ -1,7 +1,28 @@
 require 'test/unit'
 
 class TTTTest < Test::Unit::TestCase
+
+	def setup
+		@player_test = TicTacToePlayer.new "X"
+	end
+
+	def test_one_argument
+		assert_nothing_raised do 
+			@player_test.turn []
+		end
+	end
+
 end
 
 class TicTacToePlayer
+
+
+	def initialize (name)
+		@name = name
+	end
+
+	def turn(game_state)
+		raise 'Not implemented yet!'
+	end
+
 end
